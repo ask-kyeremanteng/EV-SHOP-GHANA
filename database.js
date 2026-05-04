08600997603222365dba1ecf98abcdd82a47cb48
KWADWO KYEREMANTENG <k.kyeremanteng9@gmail.com>
2026-05-04 23:51:26 +0200

    Update database.js

--- database.js
+++ database.js
@@ -2,7 +2,9 @@ const initSqlJs = require('sql.js');
 const fs = require('fs');
 const path = require('path');
 
-const DB_PATH = path.join(__dirname, 'evshop.db');
+const DB_PATH = process.env.RAILWAY_VOLUME_MOUNT_PATH
+  ? path.join(process.env.RAILWAY_VOLUME_MOUNT_PATH, 'evshop.db')
+  : path.join(__dirname, 'evshop.db');
 let db;
 
 async function getDb() {
